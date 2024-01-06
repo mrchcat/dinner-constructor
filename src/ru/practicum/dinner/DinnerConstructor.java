765dd7e41290b71c00ceb77830017d76dc1f389a
mrchcat <m-cat@mail.ru>
2024-01-06 18:35:48 +0300

    initial commit

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,62 @@
 package ru.practicum.dinner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class DinnerConstructor {
+    private HashMap<String, String> menu;
+
+    public DinnerConstructor() {
+        this.menu = new HashMap<String, HashSet<String>>();
+    }
+
+    boolean addNewDish(String dishType, String dishName) {
+        HashSet<String> dishes;
+        if (menu.containsKey(dishType)) {
+            dishes = menu.get(dishType);
+            if (dishes.contains(dishName)) return false;
+            else dishes.add(dishName);
+        } else {
+            dishes = new HashSet<>();
+            dishes.add(dishName);
+            menu.put(dishType, dishes);
+        }
+        return true;
+    }
+
+    boolean isContainDishType(String dishType) {
+        return menu.containsKey(dishType);
+    }
+
+    void printMenu() {
+        System.out.println("Меню:");
+        for (var entry : menu.entrySet()) {
+            System.out.println(entry.getKey());
+            for (String s : entry.getValue()) {
+                System.out.println("    " + s);
+            }
+        }
+    }
+
+    void generateDishCombo(int numberOfCombos, ArrayList<String> mealTypes) {
+        int totalCombinations = 1;
+        for (String dishType : mealTypes) {
+            totalCombinations *= menu.get(dishType).size();
+        }
+        if (numberOfCombos > totalCombinations) {
+            System.out.println("Требуемое число комбинаций превышает возможности меню. Добавьте больше блюд");
+            generateAll(mealTypes);
+        } else if (numberOfCombos == totalCombinations) generateAll(mealTypes);
+        else generateRandom(numberOfCombos, mealTypes);
+    }
+
+    private void generateAll(HashSet<String> mealTypes) {
+        for (String dishType : mealTypes) {
+            for(String dish: menu.get(dishType)){
+
+            }
+        }
+    }
 
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -1,32 +1,26 @@
 package ru.practicum.dinner;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-
-    static DinnerConstructor dc;
-    static Scanner scanner;
+    private static DinnerConstructor dc=new DinnerConstructor();
+    private static Scanner scanner= new Scanner(System.in);
 
     public static void main(String[] args) {
-        //
-        dc = new DinnerConstructor();
-        scanner = new Scanner(System.in);
+        boolean isContinueMenu = true;//условие на выход из меню
 
-        while (true) {
+        do {
             printMenu();
-            String command = scanner.nextLine();
-
-            switch (command) {
-                case "1":
-                    addNewDish();
-                    break;
-                case "2":
-                    generateDishCombo();
-                    break;
-                case "3":
-                    return;
+            switch (getNumberFromUser(scanner)) {
+                case 1 -> addNewDish();
+                case 2 -> generateDishCombo();
+                case 3 -> isContinueMenu = false;
             }
-        }
+        } while (isContinueMenu);
+        System.out.println("Работа завершена");
     }
 
     private static void printMenu() {
@@ -36,31 +30,53 @@ public class Main {
         System.out.println("3 - Выход");
     }
 
+    private static int getNumberFromUser(Scanner scanner) {
+        int choice = -1;
+        if (scanner.hasNextInt()) choice = scanner.nextInt();
+        if (scanner.hasNextLine()) scanner.nextLine(); // удаляем лишний текст, если пользователь его напечатал
+        return choice;
+    }
+
+    private static String getStringFromUser(Scanner scanner) {
+        return scanner.nextLine().trim().toLowerCase(Locale.ROOT);
+    }
+
     private static void addNewDish() {
         System.out.println("Введите тип блюда:");
-        String dishType = scanner.nextLine();
+        String dishType = getStringFromUser(scanner);
         System.out.println("Введите название блюда:");
-        String dishName = scanner.nextLine();
-
-        // добавьте новое блюдо
+        String dishName = getStringFromUser(scanner);
+        if (dc.addNewDish(dishType,dishName)) {
+            System.out.println("блюдо \""+dishName+"\" успешно добавлено в категорию \""+dishType+"\"");
+        }
+        else {
+            System.out.println("Ошибка ! Блюдо \""+dishName+"\" уже присутствует в категории \""+dishType+"\"");
+        }
+//        dc.printMenu();
     }
 
     private static void generateDishCombo() {
+        boolean isAnyDishesAdded=false;
         System.out.println("Начинаем конструировать обед...");
-
         System.out.println("Введите количество наборов, которые нужно сгенерировать:");
-        int numberOfCombos = scanner.nextInt();
-        scanner.nextLine();
-
+        int numberOfCombos = getNumberFromUser(scanner);
         System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
-        String nextItem = scanner.nextLine();
-
+        String dishType;
+        ArrayList<String> mealTypes=new ArrayList<>();
         //реализуйте ввод типов блюд
-        while (!nextItem.isEmpty()) {
-
+        while (!scanner.nextLine().isEmpty()) {
+            dishType= getStringFromUser(scanner);
+            if(!dc.isContainDishType(dishType)) {
+                System.out.println("Ошибка. Категория \"" +dishType+"\" отсуствует в меню. Введите другой тип");
+            }
+            else {
+                mealTypes.add(dishType);
+                isAnyDishesAdded=true;
+            }
         }
-
-        // сгенерируйте комбинации блюд и выведите на экран
-
+        if (isAnyDishesAdded) dc.generateDishCombo(numberOfCombos,mealTypes);
+        else System.out.println("Блюда не введены");
     }
+
+
 }
